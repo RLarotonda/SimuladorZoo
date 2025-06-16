e57ba8338699abc8cecd675915de6eea799340f5
Luanacosmo <cosmoluana712@gmail.com>
2025-06-16 11:33:24 -0300

    Update AnimalSaudavelException.java

--- AnimalSaudavelException.java
+++ AnimalSaudavelException.java
@@ -1,5 +1,16 @@
+/**
+ * Exceção lançada quando se tenta tratar um animal que já está saudável.
+ * Essa exceção serve para impedir tratamentos desnecessários e alertar o cuidador.
+ */
 public class AnimalSaudavelException extends RuntimeException {
+
+    /**
+     * Construtor que recebe uma mensagem de erro.
+     *
+     * @param message A mensagem explicando o motivo da exceção.
+     */
     public AnimalSaudavelException(String message) {
         super(message);
     }
 }
+
