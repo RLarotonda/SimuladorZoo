50c55c5572243751e32aafe13ce9d8b824bb426f
Luanacosmo <cosmoluana712@gmail.com>
2025-06-16 11:32:16 -0300

    Update AlimentoErradoException.java

--- AlimentoErradoException.java
+++ AlimentoErradoException.java
@@ -1,4 +1,14 @@
+/**
+ * Exceção lançada quando um animal recebe um tipo de alimento que não é apropriado para sua dieta.
+ * Essa exceção é usada para indicar que a ação de alimentar o animal foi inválida.
+ */
 public class AlimentoErradoException extends RuntimeException {
+
+    /**
+     * Construtor que recebe uma mensagem de erro.
+     *
+     * @param message A mensagem explicando o motivo da exceção.
+     */
     public AlimentoErradoException(String message) {
         super(message);
     }
