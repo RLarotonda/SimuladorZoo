4fda331b839db2168c12d99e64a72bacb2bc4116
Luanacosmo <cosmoluana712@gmail.com>
2025-06-16 11:31:01 -0300

    Update Alimento.java

--- Alimento.java
+++ Alimento.java
@@ -1,6 +1,16 @@
+/**
+ * Enum que representa os tipos de alimentos disponíveis no zoológico.
+ * Cada animal aceita apenas certos tipos de alimento. 
+ 
+ */
 public enum Alimento {
     CARNE, VEGETAIS, PEIXE;
 
+    /**
+     * Retorna o nome do alimento em letras maiúsculas.
+     *
+     * @return nome do alimento como {@code String}.
+     */
     @Override
     public String toString() {
         return name();
