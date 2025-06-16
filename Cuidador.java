5440fa39ed9c914619ffb593e8a0676cfa4e7ccb
Rafael Larotonda <trizlarotonda@gmail.com>
2025-06-15 23:32:20 -0300

    UpdateCuidador.java
    Aumento da felicidade e diminuição da fome serão realizados nas subclasses Animais

--- Cuidador.java
+++ Cuidador.java
@@ -7,12 +7,10 @@ public class Cuidador implements Comandos{
     }
     public void interagir(Animal animal) {
         animal.interagir();
-        animal.aumentarFelicidade(30);
     }
     public void alimentar(Animal animal, Alimento alimento) {
         try {
             animal.alimentar(alimento);
-            animal.diminuirFome(40);
         } catch (AlimentoErradoException e) {
             System.out.println("Erro: " + e.getMessage());
         }
